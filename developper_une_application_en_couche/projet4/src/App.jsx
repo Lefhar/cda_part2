d318cc3d0aa731bc93123bc9c5532cdefb17fcf8
harold <s.lefebvre907@laposte.net>
2022-03-04 10:28:01 +0100

    mise à jour

--- developper_une_application_en_couche/projet4/src/App.jsx
+++ developper_une_application_en_couche/projet4/src/App.jsx
@@ -2,28 +2,34 @@ import {React, useState} from 'react';
 
 const App = (props) => {
     const [listes, setListes] = useState([]);
-    const [liste, setListe] = useState("");
+    const [element, setElement] = useState("");
 
-    const handleChangeListe = (evt) => {
-        setListe(evt.target.value);
+    const handleChangesElement = (evt) => {
+        setElement(evt.target.value);
     }
 
-    return (
-        <div>
-            <div>
-                <ul>
-                    {listes.map((a, i) => (
-                        <li key={i}>{a}</li>
-                    ))}
-                </ul>
-            </div>
-            <input type="text" value={liste} onChange={handleChangeListe}/>
+    const handleChangeArray = () =>{
+        setListes((listes) => listes.concat(element))
+        setElement("");
+}
 
-            <button onClick={() => setListes((listes) => listes.concat(liste))}>
-                add
-            </button>
+return (
+    <div>
+        <div>
+            <ul>
+                {listes.map((a, i) => (
+                    <li key={i}>{a}</li>
+                ))}
+            </ul>
         </div>
-    );
+        <input type="text" value={element} onChange={handleChangesElement}/>
+
+        <button onClick={handleChangeArray}>
+        add
+    </button>
+</div>
+)
+;
 }
 
 export {App};
